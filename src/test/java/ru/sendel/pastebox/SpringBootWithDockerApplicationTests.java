5d8aa68209edd450d037e17ebf82afffa4099895
Maksim <Maks.pygamov@gmail.com>
2023-08-22 14:28:00 +0300

    start wright project

---
+++ src/test/java/ru/sendel/pastebox/SpringBootWithDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sendel.pastebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
